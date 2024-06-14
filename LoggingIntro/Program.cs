4b402e75f59193a038103d8f0d38d78e31c55ed7
Vishal D Bhat <60818841+vishalbhatd123456@users.noreply.github.com>
2024-06-14 06:36:39 +0000

    added start logger - logger extension

---
+++ LoggingIntro/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Logging;
+
+#region explore_later
+using var loggerFactory = LoggerFactory.Create(builder =>
+    builder.AddConsole();
+#pragma warning restore C1416
+});
+
+ILogger CreateLogger()
+{
+    return loggerFactory.CreateLogger("Course");
+}
+#endregion
+
+ILogger logger = CreateLogger();
