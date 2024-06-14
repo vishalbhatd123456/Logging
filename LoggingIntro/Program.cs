0df455636d6f24dc55217c0e776bd31f84600265
Vishal D Bhat <60818841+vishalbhatd123456@users.noreply.github.com>
2024-06-14 15:49:54 +0000

    added the tincture again

--- LoggingIntro/Program.cs
+++ LoggingIntro/Program.cs
@@ -22,3 +22,9 @@ logger.LogInformation("Hello world");
 logger.LogCritical("This is a critical message warning");
 logger.LogDebug("Tis is a debug message");
 logger.LogTrace("This is a trace call message");
+
+
+//The logger actually has only 2 methods more of Log as an imp one
+//the others are just decontructing the same
+
+logger.Log(LogLevel.Debug, 0, "Hello World!");
